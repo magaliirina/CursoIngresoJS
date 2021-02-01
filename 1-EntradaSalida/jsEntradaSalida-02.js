cd37f3796a9da2c18183b72cfba55db38ae60223
magaliirina <magaliinarciso@gmail.com>
2021-02-01 08:52:24 -0300

    Ejercicio 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,17 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	/*se puede escribir en una linea */
+	//let mensaje = prompt ("ingrese nombre")
+
+	let mensaje;
+	mensaje = prompt ("Ingrese su nombre")
+	/*undefined es el error que sale cuando la variable no a sido definida*/ 
+
+	
+
+	
+	/*declaro la variable e inicializo con la cadena "ingrese nombre" */
+	alert(mensaje);
 }
 
