5da65daf2548c7c40d6c1e42fbd20f32e2352506
magaliirina <magaliinarciso@gmail.com>
2021-02-01 10:30:28 -0300

    ejercicio 3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,18 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombre;
+
+	nombre = document.getElementById("txtIdNombre").value;
+	/*document: quiere decir que me lo proporciona html
+	(".."): nombre del id
+	value: es porque querimos el valor de esa casilla, donde escribira el usuario */
+
+	alert(nombre);
+
+	/* para que el nombre se borre automaticamente terminar*/
+
+	document.getElementById("txtIdNombre").value = ""
 
 }
 
