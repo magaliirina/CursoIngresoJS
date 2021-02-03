5f9c509d5d93f2b0f5fedefa6c4dc62ee7b4c8cb
magaliirina <magaliinarciso@gmail.com>
2021-02-03 20:49:36 -0300

    Ferrete Construccion

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -5,14 +5,73 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 */
 
 function Rectangulo () 
-{
+{ 
+    let largo;
+    let largoMultiplicado;
+    let ancho;
+    let anchoMultiplicado;
+    let vueltas = 3;
+    let perimetro;
+
+    //Lo paso a entero
+    largo = txtIdLargo.value;
+    largo = parseInt(largo);
+
+    //Lo multiplico
+    largoMultiplicado = largo * 2;
+
+    //Lo paso a entero
+    ancho = txtIdAncho.value;
+    ancho = parseInt(ancho);
+
+    //Lo multiplico
+    anchoMultiplicado = ancho * 2;
+
+    //Calculo el perimetro
+    perimetro = (anchoMultiplicado + largoMultiplicado) * vueltas;
+
+    alert("La cantidad necesaria de alambre es de " + perimetro + " metros.");
 
 }
 function Circulo () 
 {
-	
+    let radio;
+    let vueltas = 3;
+    // Math.PI: equivale al numero PI
+    //alert(Math.PI);
+    let Pi = Math.PI;
+    let circunferencia;
+
+    radio = txtIdRadio.value;
+    radio = parseInt(radio);
+
+    circunferencia = 2 * Pi * radio * vueltas;
+
+    alert("La cantidad necesaria de alambre es de " + circunferencia + " metros.");
+
+
 }
 function Materiales () 
 {
+    let largo;
+    let ancho;
+    let cemento = 2;
+    let cementoRequerido;
+    let cal = 3;
+    let calRequerida;
+    let superficie;
+
+    ancho = txtIdAncho.value;
+    ancho = parseInt(ancho);
+
+    largo = txtIdLargo.value;
+    largo = parseInt(largo);
+
+    superficie = ancho * largo;
+
+    cementoRequerido = superficie * cemento;
+    calRequerida = superficie * cal;
+
+    alert("Son necesarias " + calRequerida + " bolsas de cal y " + cementoRequerido + " bolsas de cemento.")
 	
 }
