1b1f647893e5cb1a50411cfe43e5c39f82c2616f
magaliirina <magaliinarciso@gmail.com>
2021-02-02 22:34:16 -0300

    Ejercicio 10

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+    let ancho;
+    let largo;
+    let perimetro;
+
+    ancho = prompt("Porfavor indique el ancho del rectángulo");
+    ancho = parseInt(ancho);
+
+    largo = prompt("Porfavor indique el largo del rectángulo");
+    largo = parseInt(largo);
+
+    perimetro = (ancho * 2) + (largo * 2);
+
+    alert("Su perimetro es " + perimetro);
 
 }
