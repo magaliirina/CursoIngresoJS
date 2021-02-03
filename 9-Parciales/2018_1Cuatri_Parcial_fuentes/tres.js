2ce4e15eba9ab702ea771d1e6f6a6aa066c8ca0e
magaliirina <magaliinarciso@gmail.com>
2021-02-03 16:53:48 -0300

    Punto tres parcial 2018

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+    let precio;
+    let descuento;
+    let descuentoFinal;
+    let precioFinal;
 
+    precio = prompt("Indique el Precio");
+    precio = parseInt(precio);
+
+    descuento = prompt("Indique el descuento ... %");
+    descuento = parseInt(descuento);
+
+    descuentoFinal = 100 - descuento;
+
+    precioFinal = descuentoFinal * precio / 100;
+
+    elPrecioFinal.value = precioFinal;
 }
