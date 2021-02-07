1f25e9ad70651be21ad5b58a17846eff9700645f
magaliirina <magaliinarciso@gmail.com>
2021-02-07 20:33:27 -0300

    if ejercicio 7 y 8

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,7 +1,14 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad; 
+	let estadoCivico;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value); 
+	estadoCivico = document.getElementById("estadoCivil").value; 
+
+	if (edad < 18 && estadoCivico != "Soltero" ){
+		alert("Usted es muy joven para NO ser soltero");
+	}
 	
 
 
