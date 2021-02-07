9cfeee75a17efda5322484ecc513a39f46f0e7d3
magaliirina <magaliinarciso@gmail.com>
2021-02-07 18:34:07 -0300

    ejercicio 6 if else

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,37 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad; 
+	edad = parseInt(document.getElementById("txtIdEdad").value);  
 
 
+	// if(edad >= 13 && edad <= 17){
+	// 	alert("Es adolecente");
+
+	// }	
+	// else{
+
+	// 	if(edad >= 18){
+				
+	// 		alert("Mayor de edad");
+	// 	}
+	// 	else{
+
+	// 		alert("Niño");
+	// 	}
+	// 	}
+
+	//Codigo mas limpio
+
+	//el if abre y cierra
+	if (edad >= 13 && edad <= 17){
+		alert("Es adolecente");
+	}	
+	else if(edad >= 18){
+		alert("Mayor de edad");
+	}
+	else{
+		alert("Niño");
+		}
+		
 
 }//FIN DE LA FUNCIÓN
