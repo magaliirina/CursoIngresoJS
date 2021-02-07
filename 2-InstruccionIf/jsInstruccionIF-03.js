b9c8bc42a0020273b2e6438bb78523b00d6819ae
magaliirina <magaliinarciso@gmail.com>
2021-02-07 14:23:13 -0300

    if Ejercicio 3

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt(txtIdEdad.value);
+
+	if( edad >= 18){
+		alert("Mayor de edad");
+	}
+
+	if(edad <= 18){
+		alert("Menor de edad");
+	}
+	alert("fin");
+
 
 }//FIN DE LA FUNCIÓN
