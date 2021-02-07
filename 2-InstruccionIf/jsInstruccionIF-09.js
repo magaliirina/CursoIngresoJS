c371e23613f4b2748db2a11d9d7cd080a906e3ca
magaliirina <magaliinarciso@gmail.com>
2021-02-07 20:46:59 -0300

    numero random

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let numero;
+	//numeros maximos y minimos de mi rango
+	let maximo = 10;
+	let minimo = 1;
+	
+	//Math.round redondea a numero mas cercano
+	//Math.random() tira un numero aleatorio entre 0 y 0.9999999
+	numero = Math.round(Math.random() * (maximo - minimo) + minimo);
 
+	alert(numero);
+	
 }//FIN DE LA FUNCIÓN
