233d3a31a930a9a1b54dc31a8948861188996ee0
magaliirina <magaliinarciso@gmail.com>
2021-02-07 21:14:21 -0300

    if ejercicio 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let numeroRandom;
+	let maximo = 10;
+	let minimo = 1;
+	let mensaje;
 
+	numeroRandom = Math.round(Math.random() * (maximo-minimo) + minimo);
+
+	if (numeroRandom >= 9){
+		mensaje = " Excelente";
+	}
+	else if (numeroRandom < 9 && numeroRandom >= 4){
+		mensaje = " APROBO";
+	}
+	else {
+		mensaje = " Vamos, la proxima se puede";
+	}
+
+	alert("Su nota fue " + numeroRandom + mensaje);
 }//FIN DE LA FUNCIÓN
