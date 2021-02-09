2ddf5e9168463ea41a00c08cc37bad6c3b20c45e
magaliirina <magaliinarciso@gmail.com>
2021-02-09 10:29:50 -0300

    Ejercicio 4 parcial2018

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
-
+    let primerNumero;
+    let segundoNumero;
+    let resta;
+    let suma;
+    
+    primerNumero = parseInt(prompt("Ingrese el primer número"));
+    segundoNumero = parseInt(prompt("Ingrese el segundo número"));
+    
+    resta = primerNumero - segundoNumero;
+    suma =  primerNumero + segundoNumero;
+    
+    if (primerNumero == segundoNumero){
+        alert("El resultado es " + primerNumero + segundoNumero);
+    }
+    else{
+    
+        if(primerNumero > segundoNumero){
+            alert("El resultado es " + resta);
+        }
+        else{
+            if(primerNumero < segundoNumero && suma <= 10){
+                alert("El resultado es " + suma );
+            }
+            else{
+                alert("El resultado es " + suma + " y supera a 10");
+            }
+        }
+    }
+    // if(primerNumero == segundoNumero){
+        //     alert("El resultado es " + primerNumero + segundoNumero);
+        // }
+        // else if(primerNumero > segundoNumero){
+            //     resta = primerNumero - segundoNumero;
+            //     alert("El resultado es " + resta);
+            // }
+            // else{
+                //     suma = primerNumero + segundoNumero;
+                //     alert("El resultado es " + suma);
+                
+                //     if(suma > 10){
+                    //         alert("supera a 10");
+    //     }
+    // }
+    
 }
