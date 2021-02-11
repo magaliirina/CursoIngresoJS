0a0ec36c7784b6d6ddd06aaeee7ce3de7f88fc6c
magaliirina <magaliinarciso@gmail.com>
2021-02-10 22:17:44 -0300

    Switch 9 optimizado

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -14,54 +14,55 @@ function mostrar()
 		case "Invierno":
 			if(destino == "Bariloche"){
 				aumento= 20;
-				aumento= aumento * precio / 100;
-				precioFinal= precio + aumento;
 			}
 			else if (destino == "Cordoba" || destino == "Cataratas"){
 				descuento= 10;
-				descuento= descuento*precio/100;
-				precioFinal= precio - descuento;
 			}
 			else{
 				descuento= 20;
-				descuento= descuento*precio/100;
-				precioFinal= precio - descuento;
 			}
-			alert(precioFinal);
 			break;
 			//Cierro Invierno
 		case "Verano":
 			if(destino == "Bariloche"){
 				descuento= 20;
-				descuento= descuento * precio / 100;
-				precioFinal= precio - descuento;
 			}
 			else if (destino == "Cordoba" || destino == "Cataratas"){
 				aumento= 10;
-				aumento= aumento*precio/100;
-				precioFinal= precio + aumento;
 			}
 			else{
 				aumento= 20;
-				aumento= aumento*precio/100;
-				precioFinal= precio + aumento;
 			}
-			alert(precioFinal);
 			break;
 			//Cierro Verano
 		case "Otoño":
 		case "Primavera":
 			if (destino != "Cordoba"){
 				aumento= 10;
-				aumento= aumento*precio/100;
-				precioFinal= precio + aumento;
+				//Optimizado ;)
+				/*aumento= aumento*precio/100;
+				precioFinal= precio + aumento;*/
 			}
 			else{
-				precioFinal= precio;
+				aumento= 0;
 			}
-			alert(precioFinal);
 			break;
 			//Cierro Otoño-Primavera
 	}
+
+	
+	if (aumento >= 0){
+		//alert(aumento);
+		aumento= precio*aumento/100;
+		//alert(precio);
+		//alert(aumento);
+		precioFinal= precio+aumento;
+	}
+	else if (descuento != 0){
+		descuento= precio*descuento/100;
+		precioFinal= precio-descuento;
+	}
+	
+	alert("Precio final " +precioFinal);
 
 }//FIN DE LA FUNCIÓN
