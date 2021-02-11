a8409a0f533644fe4f07034dde300bad48ec7d3c
magaliirina <magaliinarciso@gmail.com>
2021-02-11 15:48:11 -0300

    Switch 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,81 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let destino;
+	let estacion;
+	let mensaje;
+
+	destino= document.getElementById("txtIdDestino").value;
+	estacion= document.getElementById("txtIdEstacion").value;
+
+	switch(estacion){
+		case "Invierno":
+			if (destino== "Bariloche"){
+				alert("Se viaja");
+			}
+			else{
+				alert("No se viaja");
+			}
+			break;
+		case "Verano":
+			if (destino== "Mar del plata" || destino== "Cataratas"){
+				alert("Se viaja");
+			}
+			else{
+				alert("No se viaja");
+			}
+			break;
+		case "Primavera":
+			if (destino!= "Bariloche"){
+				alert("Se viaja");
+			}
+			else{
+				alert("No se viaja");
+			}
+			break;
+		default:
+			alert("Se viaja")
+			break;
+	}
+
+
+
+
+	/*if(estacion== "Invierno"){
+		switch(destino){
+			case "Bariloche":
+				alert("Se viaja");
+				break;
+			default:
+				alert("No se viaja");
+				break;
+		}
+	}
+	else if(estacion== "Verano"){
+		switch(destino){
+			case "Mar del plata":
+			case "Cataratas":
+				alert("Se viaja");
+				break;
+			default:
+				alert("No se viaja");
+		}
+	}
+	else if(estacion== "Primavera"){
+		switch(destino){
+			case "Mar del plata":
+			case "Cordoba":
+			case "Cataratas":
+				alert("Se viaja");
+				break;
+			default:
+				alert("No se viaja");
+		}
+	}
+	else{
+		alert("Se viaja");
+	}
+*/
+	
+
 
 }//FIN DE LA FUNCIÓN
